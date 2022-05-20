cabc058d45ddf5f03b38d7d691aaa57ab12fbf51
Francisco Charris <francisco.charris@koombea.com>
2022-05-20 10:41:28 -0500

    views application changed, routes for devise configured, regiatrations an session done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    # sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'sign_out' => 'devise/sessions#destroy'
+  end
 end
