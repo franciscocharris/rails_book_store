11cb4396f242c7bfedfcef16d857592d32b132e5
Francisco Charris <francisco.charris@koombea.com>
2022-05-23 10:15:50 -0500

    signUp and sign in done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :email, :password, :Date_birth, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
